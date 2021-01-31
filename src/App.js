7928ac170c5fd676fb21991da62b2db4fba2d989
NguyenVanVu2449 <ngvanvu2449@gmail.com>
2021-01-31 21:27:40 +0700

    ADD: Login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header/Header';
+import './styles/App.css';
+import './styles/style.css';
+import { BrowserRouter as Router ,Route} from 'react-router-dom';
+import { Login } from './screens';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <Header/> */}
+      <Route exact path="/login" component={Login}/>
+    </Router>
   );
 }
 
---
+++ src/components/LeftBar/LeftBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LeftBar() {
+    return (
+        <div>
+            hihi
+        </div>
+    )
+}
